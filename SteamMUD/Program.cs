203a850cc1a62d4debf9611a3bd02cfd68715dc8
tylerlang94 <tylerlang94@protonmail.com>
2023-12-06 20:42:35 -0700

    adding files back

---
+++ SteamMUD/Program.cs
@@ -0,0 +1,16 @@
+﻿using SteamMUD.Server;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+        TelnetServer telnet = new();
+        
+        telnet.Start("127.0.0.1", 8080);
+        Console.WriteLine("");
+        Console.ReadLine();
+        
+        telnet.Stop();
+    }
+}
